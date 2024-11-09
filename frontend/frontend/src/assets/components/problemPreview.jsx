bf0d1b3b1d611aca2111419ea07861a4e00984e1
Iris Veress <irisveress@gmail.com>
2024-11-09 10:16:48 +0200

    connected problems page with backend
    and problems page

---
+++ frontend/frontend/src/assets/components/problemPreview.jsx
@@ -0,0 +1,24 @@
+import FormattedText from "./formattedText";
+import { useNavigate } from "react-router-dom";
+
+function ProblemPreview({ subject, index }) {
+  const navigate = useNavigate();
+  const handleProblemClick = (subject) => {
+    localStorage.setItem("selectedSubject", JSON.stringify(subject));
+    navigate("/view");
+  };
+  return (
+    <div
+      key={index}
+      className="bg-lbnsw shadow-lg rounded-lg p-5 text-left space-y-2 hover:shadow-xl transition-shadow duration-300 cursor-pointer"
+      onClick={() => handleProblemClick(subject)}
+    >
+      <h3 className="text-xl font-semibold text-black mb-4">
+        {subject.creator} a generat o problemă de {subject.subject}, clasa a{" "}
+        {subject.grade}-a, faza {subject.stage}
+      </h3>
+      <FormattedText text={subject.content} className />
+    </div>
+  );
+}
+export default ProblemPreview;
