352ddb3bb130be01404f2c9134a47d7ae5caf267
Enilesi <miriam.iseline@gmail.com>
2024-11-09 19:40:34 +0200

    change bee image from problems

--- frontend/frontend/src/assets/components/problems/BeeImage.jsx
+++ frontend/frontend/src/assets/components/problems/BeeImage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Bee from "../../../assets/images/problems-images/problems-bee.png";
+import Bee from "../../../assets/images/problems-images/problems-bee.svg";
 
 const BeeImage = () => {
   return (
     <div className="absolute bottom-24 left-[2%] z-20 -mt-12 float-dramatically-animation">
-      <img src={Bee} alt="Bee" className="h-[370px] w-auto" />
+      <img src={Bee} alt="Bee" className="h-[250px] w-auto" />
     </div>
   );
 };
